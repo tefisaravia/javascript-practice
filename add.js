48f72ed6d83ae0fdebb1cbd5286da9b3e6cfff10
Tefi Saravia <tefisaraviavirtual@gmail.com>
2023-09-15 18:58:59 -0500

    exercise add

---
+++ add.js
@@ -0,0 +1,20 @@
+/* Write a function that returns the sum of two numbers.
+[input] integer param1
+
+Guaranteed constraints:
+-1000 ≤ param1 ≤ 1000.
+
+[input] integer param2
+
+Guaranteed constraints:
+-1000 ≤ param2 ≤ 1000.
+
+[output] integer
+
+The sum of the two inputs.
+ */
+
+function solution(param1, param2) {
+  let sum = param1 + param2;
+  return sum;
+}
